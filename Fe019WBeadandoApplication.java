9f80a4552cda1e53b3327fff4988380d47a0176a
Darkwolf69 <69400316+Darkwolf69@users.noreply.github.com>
2022-06-14 13:59:38 +0200

    Add files via upload

---
+++ Fe019WBeadandoApplication.java
@@ -0,0 +1,13 @@
+package com.example.FE019W_beadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fe019WBeadandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fe019WBeadandoApplication.class, args);
+	}
+
+}
